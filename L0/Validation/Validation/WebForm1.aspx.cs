565a3b68b137ba5341722ed3f997e72e95c247d7
evis <evis@DESKTOP-7KH3UO4>
2021-07-11 14:59:59 +0300

    Added sessions

--- L0/Validation/Validation/WebForm1.aspx.cs
+++ L0/Validation/Validation/WebForm1.aspx.cs
@@ -9,8 +9,10 @@ namespace Validation
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+    
         protected void Page_Load(object sender, EventArgs e)
         {
+            //set age list
             if (DropDownList1.Items.Count == 0)
             {
                 DropDownList1.Items.Add("---------- ");
@@ -19,12 +21,39 @@ namespace Validation
                 {
                     DropDownList1.Items.Add(i.ToString());
                 }
+
+                //set age field from session
+                if (Session["age"] != null)
+                {
+                    DropDownList1.Items.FindByValue((string)Session["age"]).Selected = true;
+                }
+            }
+
+            //set name field from session
+            if (Session["name"] != null)
+            {
+                TextBox1.Text = (string)Session["name"];
+            }
+
+            //set surname field from session
+            if (Session["surname"] != null)
+            {
+                TextBox2.Text = (string)Session["surname"];
+            }
+
+            //set school field from session
+            if (Session["school"] != null)
+            {
+                TextBox3.Text = (string)Session["school"];
             }
         }
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-
+            Session["name"] = TextBox1.Text;
+            Session["surname"] = TextBox2.Text;
+            Session["school"] = TextBox3.Text;
+            Session["age"] = DropDownList1.SelectedValue;
         }
     }
 }
