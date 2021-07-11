583611e65ee4ef6cdb92fb11f71137d2ff5209e8
evis <evis@DESKTOP-7KH3UO4>
2021-07-11 08:41:40 +0300

    Added validation

--- L0/Validation/Validation/WebForm1.aspx
+++ L0/Validation/Validation/WebForm1.aspx
@@ -11,13 +11,16 @@
         <div>
         </div>
         <asp:Label ID="Label1" runat="server" Text="Konkurso dalyvio registracija:"></asp:Label>
+        <asp:ValidationSummary ID="ValidationSummary1" runat="server" ForeColor="Red" />
         <br />
         <asp:Label ID="Label2" runat="server" Text="Vardas:"></asp:Label>
         <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="TextBox1" ErrorMessage="Vardas yra privalomas" ForeColor="Red">*</asp:RequiredFieldValidator>
         <br />
         <asp:Label ID="Label3" runat="server" Text="Amžius:"></asp:Label>
         <asp:DropDownList ID="DropDownList1" runat="server">
         </asp:DropDownList>
+        <asp:RangeValidator ID="RangeValidator1" runat="server" ControlToValidate="DropDownList1" ErrorMessage="Neteisinga metų reikšmė" ForeColor="Red" MaximumValue="25" MinimumValue="14" Type="Integer">*</asp:RangeValidator>
         <br />
         <asp:Label ID="Label4" runat="server" Text="Programavimo kalba:"></asp:Label>
         <asp:CheckBoxList ID="CheckBoxList1" runat="server" DataSourceID="XmlDataSource1" DataTextField="pavadinimas" DataValueField="pavadinimas">
--- L0/Validation/Validation/WebForm1.aspx.designer.cs
+++ L0/Validation/Validation/WebForm1.aspx.designer.cs
@@ -33,6 +33,15 @@ namespace Validation
         protected global::System.Web.UI.WebControls.Label Label1;
 
         /// <summary>
+        /// ValidationSummary1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ValidationSummary ValidationSummary1;
+
+        /// <summary>
         /// Label2 control.
         /// </summary>
         /// <remarks>
@@ -51,6 +60,15 @@ namespace Validation
         protected global::System.Web.UI.WebControls.TextBox TextBox1;
 
         /// <summary>
+        /// RequiredFieldValidator1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
+
+        /// <summary>
         /// Label3 control.
         /// </summary>
         /// <remarks>
@@ -67,6 +85,15 @@ namespace Validation
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
+
+        /// <summary>
+        /// RangeValidator1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RangeValidator RangeValidator1;
 
         /// <summary>
         /// Label4 control.
