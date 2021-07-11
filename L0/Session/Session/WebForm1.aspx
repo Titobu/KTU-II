860261a9e6d402f1995eaf72e179117dd1cce81a
evis <evis@DESKTOP-7KH3UO4>
2021-07-11 07:23:37 +0300

    Added session

---
+++ L0/Session/Session/WebForm1.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Session.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:Label ID="Label1" runat="server" Text="Naujas įrašas:"></asp:Label>
+        <br />
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Įvesti" />
+        <br />
+        <br />
+        Egzistuojatys įrašai:<asp:Table ID="Table1" runat="server" BorderColor="Black" BorderWidth="1px" Height="151px" Width="773px">
+        </asp:Table>
+    </form>
+</body>
+</html>
---
+++ L0/Session/Session/WebForm1.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Session
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String savedText = (string)Session["text"];
+            PutText(savedText);
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string newText = TextBox1.Text;
+            PutText(newText);
+
+            // saving value to session
+            Session["text"] = newText;
+        }
+
+        private void PutText(string newText)
+        {
+            TableCell cell = new TableCell();
+            cell.Text = newText;
+
+            TableRow row = new TableRow();
+            row.Cells.Add(cell);
+
+            Table1.Rows.Add(row);
+        }
+    }
+}
